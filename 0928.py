0011f7fa0c6f3756c26a53f5666c7c0629eb5382
mollyya <144211230+mollyya@users.noreply.github.com>
2023-10-01 16:05:29 +0800

    Add files via upload

---
+++ 0928.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct  1 13:15:51 2023
+
+@author: molly
+"""
+
+import sqlite3
+conn=sqlite3.connect('BBQ.db')
+cursor=conn.cursor()
+cursor.execute("DROP TABLE IF EXISTS meat")
+cursor.execute('''
+               CREATE TABLE IF NOT EXISTS meat(
+                   id INTEGER PRIMARY KEY,
+                   name TEXT,
+                   price INTEGER,
+                   quantity INTEGER
+                   )''')
+               
+cursor.execute("INSERT INTO meat(name,price,quantity)VALUES('chiken',30,5)")
+cursor.execute("INSERT INTO meat(name,price,quantity)VALUES('beef',55,10)")
+cursor.execute("INSERT INTO meat(name,price,quantity)VALUES('pork',40,15)")
+conn.commit()
+cursor.execute("SELECT*FROM meat")
+meat=cursor.fetchall()
+print("肉類列表:")
+for BBQ in meat:
+    print(BBQ)
+    
+cursor.execute("UPDATE meat SET price=35 WHERE name='pork'")
+conn.commit()
+cursor.execute("SELECT*FROM meat")
+meat=cursor.fetchall()
+print("肉類列表:")
+for BBQ in meat:
+    print(BBQ)
+    
+cursor.execute("UPDATE meat SET quantity=30 WHERE name='chiken'")
+conn.commit()
+cursor.execute("SELECT*FROM meat")
+meat=cursor.fetchall()
+print("肉類列表:")
+for BBQ in meat:
+    print(BBQ)
+
+cursor.execute("DELETE FROM meat WHERE price='40'")
+conn.commit()
+cursor.execute("SELECT*FROM meat")
+meat=cursor.fetchall()
+print("肉類列表:")
+for BBQ in meat:
+    print(BBQ)
+
